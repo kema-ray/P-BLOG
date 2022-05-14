9f30ff6b302b97e4d6f7bf99029a6c49b4a76db4
rachel <rachelkemuma99@gmail.com>
2022-05-14 18:46:05 +0300

    create one to many relationship

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,10 @@
-# from ensurepip import bootstrap
-# import imp
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from config import config_options
+from flask_sqlalchemy import SQLAlchemy
 
 bootstrap = Bootstrap()
+db =  SQLAlchemy()
 
 def create_app(config_name):
     app = Flask(__name__)
@@ -13,6 +13,7 @@ def create_app(config_name):
 
     #initializing flask extensions
     bootstrap.init_app(app)
+    db.init_app(app)
 
     #registering the blueprint
     from .main import main as main_blueprint
---
+++ app/models.py
@@ -0,0 +1,22 @@
+from turtle import title
+from . import db
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255))
+    blogs = db.relationship('Blog',backref='user',lazy='dynamic')
+
+    def __repr__(self):
+        return f'User {self.username}'
+
+class Blog(db.Model):
+    __tablename__="blogs"
+
+    id = db.Column(db.Integer,primary_key=True)
+    title = db.Column(db.String(255),nullable = False)
+    user_id = db.Column(db.Integer,db.ForeignKey('users.id'))
+    
+    def __repr__(self):
+        return f'Blog {self.title}'
+
