3d6d0dec0c197d8e3cdff6c69d97ac0fa83e1238
rachel <rachelkemuma99@gmail.com>
2022-05-15 09:29:39 +0300

    create an authentication blueprint to handle all applicatication authentication requests

--- app/__init__.py
+++ app/__init__.py
@@ -18,5 +18,7 @@ def create_app(config_name):
     #registering the blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
 
     return app
