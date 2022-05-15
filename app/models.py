c10ffe1b1aae3348369db723f1cb0d2d02396867
rachel <rachelkemuma99@gmail.com>
2022-05-15 09:09:43 +0300

    add a migration and templates folder

--- app/models.py
+++ app/models.py
@@ -1,3 +1,5 @@
+import email
+from enum import unique
 from . import db
 from werkzeug.security import generate_password_hash,check_password_hash
 
@@ -10,7 +12,20 @@ class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255))
+    email = db.Column(db.String(255),unique = True,nullable = False)
+    secure_password = db.Column(db.String(255),nullable = False)
     blogs = db.relationship('Blog',backref='user',lazy='dynamic')
+
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.secure_password = generate_password_hash(password)
+
+    def verify_password(self,password):
+        return check_password_hash(self.secure_password,password)
 
     def __repr__(self):
         return f'User {self.username}'
