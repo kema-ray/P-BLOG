5306d14b00fa0cff2aedd2396a6c2e685b5f2e4a
rachel <rachelkemuma99@gmail.com>
2022-05-15 10:37:18 +0300

    Create a registration form

--- app/main/views.py
+++ app/main/views.py
@@ -1,9 +1,11 @@
 # from turtle import title
+from crypt import methods
 from flask import render_template
 
 from app.request import get_quote
 from . import main
 from ..request import get_quote
+from flask_login import login_required
 
 
 
@@ -14,6 +16,10 @@ def index():
 
     return render_template('index.html',title=title,quote=header)
 
+@main.route('/blog/new',methods=["GET","POST"])
+@login_required
+def new_blog():
 
+    return render_template('new_blog.html')
 
 
--- app/models.py
+++ app/models.py
@@ -1,14 +1,17 @@
-import email
-from enum import unique
-from . import db
+from . import db,login_manager
 from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import UserMixin
 
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 class Quote:
     def __init__(self,quote,author):
         self.quote=quote
         self.author=author
 
-class User(db.Model):
+class User(UserMixin,db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255))
